eaa8336ead9b4aa154b6a6d1f902057b9d5b7fa7
denischmkh <avraam.l2004@gmail.com>
2026-03-01 17:16:31 +0100

    Change

--- app/main.py
+++ app/main.py
@@ -34,9 +34,6 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         redis = aioredis.from_url(os.getenv("REDIS_URL"))
         FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-        # await conn.run_sync(Base.metadata.drop_all)
-        # await conn.run_sync(Base.metadata.drop_all)
-        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
 
